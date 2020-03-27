5b25a256babbd983850a0fc648bc9f68cfceec2a
foreign-skies <56491559+foreign-skies@users.noreply.github.com>
2020-03-27 08:33:15 -0600

    Got Google Map to Display

--- app/src/main/java/com/example/UI/MainActivity.java
+++ app/src/main/java/com/example/UI/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 //        login_fragment = new LoginFragment();
 //        fragment_manager = this.getSupportFragmentManager();
 //        fragment_manager.beginTransaction().add(R.id.container, login_fragment).commit();
--- app/src/main/java/com/example/UI/MapFragment.java
+++ app/src/main/java/com/example/UI/MapFragment.java
@@ -9,21 +9,40 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.UI.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     public MapFragment() {
         // Required empty public constructor
     }
 
+    GoogleMap map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+         mapFragment.getMapAsync(this);
+         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+    map = googleMap;
+    LatLng Maharashtra = new LatLng(19.169257,73.3431601);
+    map.addMarker(new MarkerOptions().position(Maharashtra).title("Maharashtra"));
+    map.moveCamera(CameraUpdateFactory.newLatLng(Maharashtra));
     }
 }
