1713439d14cf1e80b2c72e725e606be5fdc585d9
foreign-skies <56491559+foreign-skies@users.noreply.github.com>
2020-03-28 07:36:48 -0600

    Search and Settings Menu Bar with Empty Activities

--- app/src/main/java/com/example/UI/MainActivity.java
+++ app/src/main/java/com/example/UI/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.UI;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private LoginFragment login_fragment;
     private MapFragment map_fragment;
     private FragmentManager fragment_manager;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.map_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_search)
+        {
+            Toast.makeText(this,"Search Called", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+            startActivity(intent);
+        }
+        if (item.getItemId() == R.id.action_settings)
+        {
+            Toast.makeText(this,"Settings Called", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
